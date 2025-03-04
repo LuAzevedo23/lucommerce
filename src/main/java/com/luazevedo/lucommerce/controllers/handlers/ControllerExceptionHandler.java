a1efa36ce1454c77d2d4c19931d2a514907f7255
Luciene Azevedo <luciene.azevedo@hotmail.com>
2025-03-04 15:28:23 -0300

    Bean Validation

--- src/main/java/com/luazevedo/lucommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/luazevedo/lucommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.luazevedo.lucommerce.controllers.handlers;
 
 import com.luazevedo.lucommerce.dto.CustomError;
+import com.luazevedo.lucommerce.dto.ValidationError;
 import com.luazevedo.lucommerce.services.exceptions.DatabaseException;
 import com.luazevedo.lucommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,6 +30,19 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation (MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;//= erro 400
+        ValidationError err = new ValidationError (Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        //for abaixo é uma lista de todos os erros previstos nas anotations do ProductDTO
+        for (FieldError f: e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
+
+    }
+    /*
     // Captura exceções genéricas para evitar erro 500 inesperado
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CustomError> genericException(Exception e, HttpServletRequest request) {
@@ -34,6 +50,8 @@ public class ControllerExceptionHandler {
         CustomError err = new CustomError(Instant.now(), status.value(), "Erro interno no servidor", request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+    */
+
 }
 //Com a anotação @ControllerAdvice, podemos definir tratamentos globais para exceções específicas, sem precisas
 //ficar colocando o try-catch em várias partes do código.
